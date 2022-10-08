e7ab6a7e599d54813573d71dbd6475bf1134ef23
misha0203 <misha0203@gmail.com>
2022-10-08 08:38:54 +0600

    First Commit

---
+++ venv/lessons/less1.py
@@ -0,0 +1,11 @@
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def a(self):
+        print('my name is ' + self.name)
+
+    def age1(self,a):
+        print(self.age+a)
+
