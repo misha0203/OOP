db0c30052fce036086097c5774eca16d67a7aab5
misha0203 <misha0203@gmail.com>
2022-10-18 13:46:36 +0600

    18.10.2022

---
+++ home/homewr4.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+speed(8)
+color('green')
+bgcolor('black')
+i = 500
+while i > 0:
+    left(i)
+    forward(i * 3)
+    i = i - 1
---
+++ person/models.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, last, age):
+        self.name = name
+        self.last = last
+        self.age = age
+
+    def full(self):
+        return f'{self.name} {self.last} {self.age}'
