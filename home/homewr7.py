1cfe64acd346108a0d62d48039a097f18a86e820
misha0203 <misha0203@gmail.com>
2022-10-31 14:36:06 +0600

    Homework 7

--- home/homewr7.py
+++ home/homewr7.py
@@ -1,25 +1,23 @@
 def get_list() -> list:
-    return list(range(0, 1000, 2))
-def search(arr, low, high, desired):
-    if high >= low:
-        mid = (high + low) // 2
-        if arr[mid] == desired:
-            return mid
-        elif arr[mid] > desired:
-            return search(arr, low, mid - 1, desired)
-        else:
-            return search(arr, mid + 1, high, desired)
-    else:
-        return -1
-desired = int(input('Введите четное число: '))
+    return list(range(0, 100, 2))
+list = get_list()
+
 class Solution:
-    def find_target(self):
-        target = desired
-        list = get_list()
-        result = search(list,0,len(list)-1, target)
-        if result != -1:
-            print(f'Введенное число {target} имеет индекс {result} в последовательности\n {list}')
-        else:
-            print(f'Введенное число не представлено в последовательности\n {list}')
-a = Solution()
-a.find_target()
+    # list = get_list()
+    def __init__(self):...
+    def find_target(self, list, target):
+        low = 0
+        high = len(list) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if list[mid] == target:
+                return mid
+            elif list[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+
+res = Solution()
+print(res.find_target(list, 8))
